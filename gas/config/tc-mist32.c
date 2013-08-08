d7ae9691bfc0a3232ed5885e4c71e84660e34df3
Hirotaka Kawata <hirotaka@techno-st.net>
2013-08-08 12:27:22 +0900

    bug fixed: elf relocation

--- gas/config/tc-mist32.c
+++ gas/config/tc-mist32.c
@@ -175,10 +175,20 @@ md_pcrel_from_section (fixS * fixP, segT sec)
 {
   if (fixP->fx_addsy != (symbolS *) NULL
       && (! S_IS_DEFINED (fixP->fx_addsy)
-	  || S_GET_SEGMENT (fixP->fx_addsy) != sec))
+	  || S_GET_SEGMENT (fixP->fx_addsy) != sec
+          || S_IS_EXTERNAL (fixP->fx_addsy)
+          || S_IS_WEAK (fixP->fx_addsy)))
+    {
+      if (S_GET_SEGMENT (fixP->fx_addsy) != sec
+          && S_IS_DEFINED (fixP->fx_addsy)
+          && ! S_IS_EXTERNAL (fixP->fx_addsy)
+          && ! S_IS_WEAK (fixP->fx_addsy))
+        return fixP->fx_offset;
+
     /* The symbol is undefined (or is defined but not in this section).
        Let the linker figure it out. */
     return 0;
+  }
 
   return (fixP->fx_frag->fr_address + fixP->fx_where) & ~3;
 }
@@ -192,12 +202,6 @@ md_cgen_lookup_reloc (const CGEN_INSN *    insn ATTRIBUTE_UNUSED,
 		      const CGEN_OPERAND * operand,
 		      fixS *               fixP)
 {
-  /*
-    case MIST32_OPERAND_xx:
-    fixP->fx_pcrel = 1;
-    return BFD_RELOC_MIST32_xx;
-  */
-
   fixP->fx_pcrel = 0;
 
   switch (operand->type)
